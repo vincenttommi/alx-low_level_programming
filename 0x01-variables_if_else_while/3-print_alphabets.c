9a2904a8e77be681d79ab4458085eca182bfeba3
vincenttommi <vincenttommi@gmail.com>
2022-07-03 00:25:13 -0700

    alphabetics

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - print both lowercase and uppercase letters in alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+

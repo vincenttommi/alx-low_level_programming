ca7d95a9f2191608240cf5cf016ecccf4a043582
vincenttommi <vincenttommi@gmail.com>
2022-07-11 10:10:02 -0700

    stdout

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the charactor string
+ * @s:  pointer to be printed
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}

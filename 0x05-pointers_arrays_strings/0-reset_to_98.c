16d78528bcf55c6c4789e9483174dd83406e850d
vincenttommi <vincenttommi@gmail.com>
2022-07-11 09:48:01 -0700

    fix errors

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes in pointer to an int and updates it to 98
+ * @author Abdihakim Abdirahman
+ * @n: puntero
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+int x;
+x = 98;
+*n = x;
+}
